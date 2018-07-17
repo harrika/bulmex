47ee64f8cad3c2c3b732e27fd9f6d343a984e8b2
harrika <henry.kirya@gmail.com>
2018-07-17 15:12:26 +0300

    adding session

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
-
 const express = require('express');
 const path = require('path');
 const app = express();
+const session = require('express-session');
+const bodyParser = require('body-parser');
+
 
 app.set('views', path.join(__dirname,'views'));
 app.set('view engine', 'ejs');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(session({secret: 'this is the story' }));
 
 app.use(express.static(path.join(__dirname,'static')));
 var {books, fruit} = require("./data");
